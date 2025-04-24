efea45edcd66b8267a1d56f3d8bd3a855d1f7d3f
Noxwya <aidenyoko62@gmail.com>
2025-04-24 10:17:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Import all components and utilities
+import './components/cursor.js';
+import './components/navigation.js';
+import './components/animation.js';
+import './components/video.js';
+import './components/form.js';
+import './components/projects.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// Update current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
